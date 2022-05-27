b73533e0fbf79f8a522c7d7aac93de08f4b7d548
unfoundbug <iam@theadmin.co.uk>
2022-05-27 21:20:14 +0100

    Enable release/debug publishing

--- PrepPublish.ps1
+++ PrepPublish.ps1
@@ -1,14 +1,28 @@
+param([string] $buildType)
+
+Write-Host "Cleaning existing item"
 $newOutDir = Join-Path $env:APPDATA "\SpaceEngineers\Mods\UnFoundBug.AutoRecharge\"
-Remove-Item "$newOutDir" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -LiteralPath "$newOutDir" -Recurse -Force
 
-mkdir "$newOutDir"
+$nDir = mkdir "$newOutDir"
 
+Write-Host "Copying Bulk"
 Get-ChildItem .\* -File | Foreach-Object { Copy-Item -Path "$($_.FullName)" -Destination "$(Join-Path $newOutDir $($_.Name))" }
 
-mkdir "$newOutDir\Data"
-mkdir "$newOutDir\Data\Scripts"
-mkdir "$newOutDir\Data\Scripts\AutoRecharge"
+Write-Host "Copying Scripts"
+
+$nDir = mkdir "$newOutDir\Data"
+$nDir = mkdir "$newOutDir\Data\Scripts"
+$nDir = mkdir "$newOutDir\Data\Scripts\AutoRecharge"
 Get-ChildItem .\Data\*.sbc -File | Foreach-Object { Copy-Item -Path "$($_.FullName)" -Destination "$(Join-Path $newOutDir "Data" )"}
-mkdir "$newOutDir\Data\Scripts"
-mkdir "$newOutDir\Data\Scripts\AutoRecharge"
 Get-ChildItem .\Data\Scripts\AutoRecharge\*.cs -File | Foreach-Object { Copy-Item -Path "$($_.FullName)" -Destination "$(Join-Path $newOutDir "Data\Scripts\AutoRecharge\" )"}
+
+Write-Host "Activating build info"
+if ($buildType -eq "Debug") {
+	Write-Host "using debug mod info";
+	remove-item "$(Join-Path $newOutDir "modinfo_Release.sbmi")"
+} else {
+	Write-Host "using release mod info";
+	remove-item "$(Join-Path $newOutDir "modinfo.sbmi")"
+	move-item "$(Join-Path $newOutDir "modinfo_Release.sbmi")" "$(Join-Path $newOutDir "modinfo.sbmi")"
+}
